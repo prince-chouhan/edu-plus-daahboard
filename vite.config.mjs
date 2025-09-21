abb3fb45853c5b3afce3e3ca1aa4d708cf0d9d38
prince-chouhan <princechouhan1708@gmail.com>
2025-09-21 16:21:30 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,7 +7,7 @@ import tagger from "@dhiwise/component-tagger";
 export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
-  base: "/"
+  base: "/",
   build: {
     outDir: "build",
     chunkSizeWarningLimit: 2000,
@@ -21,3 +21,4 @@ export default defineConfig({
   }
 
 });
+
