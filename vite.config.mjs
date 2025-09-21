e605eb4b0c5eae0045a472602656be1b2440f7e0
prince-chouhan <princechouhan1708@gmail.com>
2025-09-21 16:27:25 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,20 +5,16 @@ import tagger from "@dhiwise/component-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
   base: "/",
   build: {
-    outDir: "build",
-    chunkSizeWarningLimit: 2000,
+    outDir: "dist",                  // <-- changed to "dist"
+    chunkSizeWarningLimit: 2000
   },
   plugins: [tsconfigPaths(), react(), tagger()],
   server: {
-    port: "4028",
+    port: 4028,                      // use a number here (cleaner)
     host: "0.0.0.0",
     strictPort: true,
     allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
   }
-
 });
-
